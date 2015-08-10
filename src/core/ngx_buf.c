0953798ba284fdb960437299a8fc7993fc08a38e
Neal <lrita@163.com>
2015-08-10 16:55:40 +0800

    增加数据结构buf/chain的注释

--- src/core/ngx_buf.c
+++ src/core/ngx_buf.c
@@ -7,10 +7,11 @@
 
 #include <ngx_config.h>
 #include <ngx_core.h>
+//该模块简介：http://blog.csdn.net/chen19870707/article/details/40866533
 
 
 ngx_buf_t *
-ngx_create_temp_buf(ngx_pool_t *pool, size_t size)
+ngx_create_temp_buf(ngx_pool_t *pool, size_t size)//创建temporary字段为1的buf（就是其内容可以被后续的filter模块进行修改）
 {
     ngx_buf_t *b;
 
@@ -45,11 +46,17 @@ ngx_create_temp_buf(ngx_pool_t *pool, size_t size)
 
 
 ngx_chain_t *
-ngx_alloc_chain_link(ngx_pool_t *pool)
+ngx_alloc_chain_link(ngx_pool_t *pool)	//创建一个ngx_chain_t的对象，并返回指向对象的指针，失败返回NULL
 {
     ngx_chain_t  *cl;
 
-    cl = pool->chain;
+    cl = pool->chain;			/*对ngx_chaint_t类型的释放，并不是真的释放了内存，
+					 *而仅仅是把这个对象挂在了这个pool对象的一个叫做
+					 *chain的字段对应的chain上，以供下次从这个pool上
+					 *分配ngx_chain_t类型对象的时候，快速的从这个pool->chain
+					 *上取下链首元素就返回了.当然，如果这个链是空的，
+					 *才会真的在这个pool上使用ngx_palloc函数进行分配
+					 */
 
     if (cl) {
         pool->chain = cl->next;
@@ -66,7 +73,7 @@ ngx_alloc_chain_link(ngx_pool_t *pool)
 
 
 ngx_chain_t *
-ngx_create_chain_of_bufs(ngx_pool_t *pool, ngx_bufs_t *bufs)
+ngx_create_chain_of_bufs(ngx_pool_t *pool, ngx_bufs_t *bufs)//创建bufs->num个连接chain，每个chain结构包含一个bufs->size大小的buf
 {
     u_char       *p;
     ngx_int_t     i;
@@ -124,14 +131,14 @@ ngx_create_chain_of_bufs(ngx_pool_t *pool, ngx_bufs_t *bufs)
 
 
 ngx_int_t
-ngx_chain_add_copy(ngx_pool_t *pool, ngx_chain_t **chain, ngx_chain_t *in)
+ngx_chain_add_copy(ngx_pool_t *pool, ngx_chain_t **chain, ngx_chain_t *in)//将in缓冲区拷贝增加到chain缓冲区末尾,但并不产生多份buf
 {
     ngx_chain_t  *cl, **ll;
 
     ll = chain;
 
     for (cl = *chain; cl; cl = cl->next) {
-        ll = &cl->next;
+        ll = &cl->next;		//找到缓冲区末尾
     }
 
     while (in) {
@@ -153,7 +160,7 @@ ngx_chain_add_copy(ngx_pool_t *pool, ngx_chain_t **chain, ngx_chain_t *in)
 
 
 ngx_chain_t *
-ngx_chain_get_free_buf(ngx_pool_t *p, ngx_chain_t **free)
+ngx_chain_get_free_buf(ngx_pool_t *p, ngx_chain_t **free)//得到链表中未使用的buf，如果没有，则分配一个
 {
     ngx_chain_t  *cl;
 
@@ -186,6 +193,7 @@ ngx_chain_update_chains(ngx_pool_t *p, ngx_chain_t **free, ngx_chain_t **busy,
 {
     ngx_chain_t  *cl;
 
+    //让busy指向out
     if (*busy == NULL) {
         *busy = *out;
 
@@ -201,19 +209,19 @@ ngx_chain_update_chains(ngx_pool_t *p, ngx_chain_t **free, ngx_chain_t **busy,
         cl = *busy;
 
         if (ngx_buf_size(cl->buf) != 0) {
-            break;
+            break;				//这个结点内存有占用，不满足释放条件，跳出
         }
 
-        if (cl->buf->tag != tag) {
+        if (cl->buf->tag != tag) {		//缓冲区类型不同，直接释放
             *busy = cl->next;
             ngx_free_chain(p, cl);
             continue;
         }
 
-        cl->buf->pos = cl->buf->start;
+        cl->buf->pos = cl->buf->start;		//将该结点pos、last初始化
         cl->buf->last = cl->buf->start;
 
-        *busy = cl->next;
+        *busy = cl->next;			//将该节点放入free
         cl->next = *free;
         *free = cl;
     }
