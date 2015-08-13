a9778310e33a7f2bf40fb9fa4aaa09252fcc1dc3
Neal <lrita@163.com>
2015-08-13 14:02:44 +0800

    增加http handler返回值的注释

--- src/core/ngx_core.h
+++ src/core/ngx_core.h
@@ -33,13 +33,18 @@ typedef void (*ngx_event_handler_pt)(ngx_event_t *ev);
 typedef void (*ngx_connection_handler_pt)(ngx_connection_t *c);
 
 
-#define  NGX_OK          0
-#define  NGX_ERROR      -1
-#define  NGX_AGAIN      -2
-#define  NGX_BUSY       -3
-#define  NGX_DONE       -4
-#define  NGX_DECLINED   -5
-#define  NGX_ABORT      -6
+#define  NGX_OK          0	//表示成功。Nginx会执行该请求的后续动作(如执行subrequest或撤销这个请求)
+#define  NGX_ERROR      -1	//表示错误，可能会调用ngx_http_terminate_request终止请求。如果还有POST子请求，那么将会执行完
+				//POST请求再终止本次请求
+
+#define  NGX_AGAIN      -2	//
+#define  NGX_BUSY       -3	//
+#define  NGX_DONE       -4	//表示到此为止，同时HTTP框架模块将暂时不再继续执行这个请求的后续操作部分
+				//事实上，此时会检查链接类型，如果是keepalive类型的用户请求，就会保持住HTTP链接，然后把控制
+				//权交给Nginx。
+
+#define  NGX_DECLINED   -5	//继续在NGX_HTTP_CONTENT_PHASE阶段寻找下一个对该请求感兴趣的HTTP模块来再次处理这个请求
+#define  NGX_ABORT      -6	//
 
 
 #include <ngx_errno.h>
