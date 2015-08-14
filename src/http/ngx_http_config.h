e80364d1608e61007bb34c5b6a36cb20acfdedf8
Neal <lrita@163.com>
2015-08-14 13:34:33 +0800

    增加HTTP CONFIG数据结构注释

--- src/http/ngx_http_config.h
+++ src/http/ngx_http_config.h
@@ -15,36 +15,40 @@
 
 
 typedef struct {
-    void        **main_conf;
-    void        **srv_conf;
-    void        **loc_conf;
+    void        **main_conf;//指针数组，数组中每个元素指向所有HTTP模块中create_main_conf方法产生的结构提
+    void        **srv_conf; //指针数组，数组中每个元素指向所有HTTP模块中create_srv_conf方法产生的结构提
+    void        **loc_conf; //指针数组，数组中每个元素指向所有HTTP模块中create_loc_conf方法产生的结构提
 } ngx_http_conf_ctx_t;
 
 
 typedef struct {
-    ngx_int_t   (*preconfiguration)(ngx_conf_t *cf);
-    ngx_int_t   (*postconfiguration)(ngx_conf_t *cf);
+    ngx_int_t   (*preconfiguration)(ngx_conf_t *cf);	//解析配置文件前调用，如果返回失败，会终止进程
+    ngx_int_t   (*postconfiguration)(ngx_conf_t *cf);	//完成配置文件解析后调用
 
-    void       *(*create_main_conf)(ngx_conf_t *cf);
-    char       *(*init_main_conf)(ngx_conf_t *cf, void *conf);
+    void       *(*create_main_conf)(ngx_conf_t *cf);	//当需要创建数据结构用户存储main级别(直属于http{...}块的配置)的全局
+    							//配置项时,可以通过create_main_conf回调方法创建储存全局配置的结构体
 
-    void       *(*create_srv_conf)(ngx_conf_t *cf);
-    char       *(*merge_srv_conf)(ngx_conf_t *cf, void *prev, void *conf);
+    char       *(*init_main_conf)(ngx_conf_t *cf, void *conf);//常用户创建main配置项
 
-    void       *(*create_loc_conf)(ngx_conf_t *cf);
-    char       *(*merge_loc_conf)(ngx_conf_t *cf, void *prev, void *conf);
+    void       *(*create_srv_conf)(ngx_conf_t *cf);	//当需要创建数据结构用户存储srv级别(直属于虚拟机server{...}块的配置)
+    							//的配置项时,可以通过create_srv_conf回调方法创建储存srv配置的结构体
+    char       *(*merge_srv_conf)(ngx_conf_t *cf, void *prev, void *conf);//用于合并srv和main级别下同名配置项
+
+    void       *(*create_loc_conf)(ngx_conf_t *cf);	//当需要创建数据结构用户存储loc级别(直属于location{...}块的配置)的全
+    							//局配置项时,可以通过create_loc_conf回调方法创建储存全局配置的结构体
+    char       *(*merge_loc_conf)(ngx_conf_t *cf, void *prev, void *conf);//用于合并srv和loc级别下同名配置项
 } ngx_http_module_t;
 
 
 #define NGX_HTTP_MODULE           0x50545448   /* "HTTP" */
 
-#define NGX_HTTP_MAIN_CONF        0x02000000
-#define NGX_HTTP_SRV_CONF         0x04000000
-#define NGX_HTTP_LOC_CONF         0x08000000
-#define NGX_HTTP_UPS_CONF         0x10000000
-#define NGX_HTTP_SIF_CONF         0x20000000
-#define NGX_HTTP_LIF_CONF         0x40000000
-#define NGX_HTTP_LMT_CONF         0x80000000
+#define NGX_HTTP_MAIN_CONF        0x02000000	//配置可以出现在http{}块内
+#define NGX_HTTP_SRV_CONF         0x04000000	//配置可以出现在http{}块内的server{}块里
+#define NGX_HTTP_LOC_CONF         0x08000000	//配置可以出现在http{}块内的location{}块里
+#define NGX_HTTP_UPS_CONF         0x10000000	//配置可以出现在http{}块内的upstream{}块里
+#define NGX_HTTP_SIF_CONF         0x20000000	//配置可以出现在http{}块内的server{}块里的if{}块中，用于rewrite
+#define NGX_HTTP_LIF_CONF         0x40000000	//配置可以出现在http{}块内的location{}块里if{}块中，用于rewrite
+#define NGX_HTTP_LMT_CONF         0x80000000	//配置可以出现在http{}块内的limit_except{}块中
 
 
 #define NGX_HTTP_MAIN_CONF_OFFSET  offsetof(ngx_http_conf_ctx_t, main_conf)
