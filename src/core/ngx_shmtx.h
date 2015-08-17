037cdb93d159c4713d2a9ac8c3b48970ac7f0e73
Neal <lrita@163.com>
2015-08-17 14:57:42 +0800

    增加进程共享锁的注释

--- src/core/ngx_shmtx.h
+++ src/core/ngx_shmtx.h
@@ -14,26 +14,26 @@
 
 
 typedef struct {
-    ngx_atomic_t   lock;
+    ngx_atomic_t   lock;	//是否处于lock状态
 #if (NGX_HAVE_POSIX_SEM)
-    ngx_atomic_t   wait;
+    ngx_atomic_t   wait;	//在等待这个锁的次数
 #endif
-} ngx_shmtx_sh_t;
+} ngx_shmtx_sh_t;	//共享内存使用的共享内存的封装
 
 
 typedef struct {
 #if (NGX_HAVE_ATOMIC_OPS)
-    ngx_atomic_t  *lock;
-#if (NGX_HAVE_POSIX_SEM)
-    ngx_atomic_t  *wait;
-    ngx_uint_t     semaphore;
-    sem_t          sem;
+    ngx_atomic_t  *lock;	//lock指针，指向ngx_shmtx_sh_t->lock
+#if (NGX_HAVE_POSIX_SEM)	//使用信号量时，信号量辅助处理lock等待操作
+    ngx_atomic_t  *wait;	//wait指针，指向ngx_shmtx_sh_t->wait
+    ngx_uint_t     semaphore;	//是否使用信号量
+    sem_t          sem;		//信号量结构提
 #endif
 #else
-    ngx_fd_t       fd;
-    u_char        *name;
+    ngx_fd_t       fd;		//使用文件锁时的文件描述符
+    u_char        *name;	//文件名称
 #endif
-    ngx_uint_t     spin;
+    ngx_uint_t     spin;	//跟lock时等待时间相关
 } ngx_shmtx_t;
 
 
